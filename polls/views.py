60b04076d57f9e7933df55f0ea1e3eb1049043c7
thyagostall <thstall@gmail.com>
2016-10-28 00:52:39 -0200

    All code working

--- polls/views.py
+++ polls/views.py
@@ -21,7 +21,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -29,7 +30,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        render(request, 'detail.html', {
+        return render(request, 'detail.html', {
             'question': question,
             'error_message': "You didn't select a choice",
         })

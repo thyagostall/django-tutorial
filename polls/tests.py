80fb345ff5160181586f3d3c711a43c0661bc67e
thyagostall <thstall@gmail.com>
2016-10-28 17:20:37 -0200

    View tests

--- polls/tests.py
+++ polls/tests.py
@@ -50,6 +50,20 @@ class QuestionViewTests(TestCase):
         )
 
 
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        future_question = create_question("Future question.", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        past_question = create_question("Past question.", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 class QuestionMethodTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         future_date = timezone.now() + timezone.timedelta(days=30)

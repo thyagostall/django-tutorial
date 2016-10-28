671dae37797d2c5915b5f889ffba662e55afa602
thyagostall <thstall@gmail.com>
2016-10-28 01:39:39 -0200

    All test cases for was_published_recently

--- polls/tests.py
+++ polls/tests.py
@@ -9,3 +9,13 @@ class QuestionMethodTests(TestCase):
         future_date = timezone.now() + timezone.timedelta(days=30)
         future_question = Question(pub_date=future_date)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        old_date = timezone.now() - timezone.timedelta(days=30)
+        old_question = Question(pub_date=old_date)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        now = timezone.now() - timezone.timedelta(hours=1)
+        recent_question = Question(pub_date=now)
+        self.assertIs(recent_question.was_published_recently(), True)

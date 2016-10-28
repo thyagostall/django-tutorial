0f3c0f3de4d9346e3036987b93cbdf510da895b6
thyagostall <thstall@gmail.com>
2016-10-28 01:34:54 -0200

    First test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from polls.models import Question
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        future_date = timezone.now() + timezone.timedelta(days=30)
+        future_question = Question(pub_date=future_date)
+        self.assertIs(future_question.was_published_recently(), False)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,3 @@
-from django.http import Http404
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse

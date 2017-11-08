ccaa97ba4f6aa8ed791b43e7a2aaf1c0ddc9d813
thyagostall <thstall@gmail.com>
2017-11-07 22:50:16 -0200

    First test + first bug

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
+import datetime
+
+from django.utils import timezone
+
 from django.test import TestCase
 
-# Create your tests here.
+from polls.models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertFalse(future_question.was_published_recently())

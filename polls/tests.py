e95435468be4845dabd364dab47dc4278ae3c7f7
thyagostall <thstall@gmail.com>
2017-11-09 11:07:51 -0200

    404 on future question detail

--- polls/tests.py
+++ polls/tests.py
@@ -94,6 +94,24 @@ class QuestionIndexViewTests(TestCase):
         )
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question', days=30)
+
+        url = reverse('polls:detail', args=(future_question.id, ))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Future question', days=-30)
+
+        url = reverse('polls:detail', args=(past_question.id, ))
+        response = self.client.get(url)
+
+        self.assertContains(response, past_question.question_text)
+
+
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
